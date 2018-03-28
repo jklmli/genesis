5847207ac9726e0807937fa940d09c10ddbb2db5
Kevin Li <jiawei.h.li@gmail.com>
2018-03-28 00:17:25 -0700

    build(jake/test): add task to report code coverage

--- jakelib/tasks/test.ts
+++ jakelib/tasks/test.ts
@@ -9,3 +9,13 @@ task('test', () => {
     npx webpack --config webpack/test.webpack.config.ts --progress && nyc ava --verbose
   `]);
 });
+
+namespace('test', () => {
+  desc('Report coverage');
+  task('report', () => {
+    execWithLog([`
+      echo '${emoji.mailbox_with_mail}  Reporting test coverage results...' &&
+      npx -c 'nyc report --reporter=text-lcov | coveralls'
+    `]);
+  });
+});
