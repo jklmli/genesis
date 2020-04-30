37de37f32caf9f42e0503483a27d2a376e161f0e
Kevin Li <jiawei.h.li@gmail.com>
2020-04-30 14:16:39 -0700

    add support for fullstory

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@
 const logRocket: any = require('logrocket');
 logRocket.init('rhq1s4/rapid');
 
+/* tslint:disable:no-any */
+const fullstory: any = require('@fullstory/browser');
+fullstory.init({ orgId: 'TWM20' });
+
 import { AppView } from './components/views/app_view';
 
 new AppView()
